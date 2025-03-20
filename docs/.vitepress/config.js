1bc02a0647ab9362dd93aa0bb36b9466c088728b
ZhimaNB <zhima@mail.sb>
2025-03-20 17:35:10 +0800

    Ver.1.0.0.0

---
+++ docs/.vitepress/config.js
@@ -0,0 +1,21 @@
+export default {
+    themeConfig: {
+      siteTitle: "C++社区中文教学",
+      logo: "/logo.png",
+      title: 'C++社区中文教学',
+      language: 'zh-CN',
+      description: 'C++教程',
+      sidebar: [
+      {
+        text: "认识计算机",
+        items: [
+          { text: "认识鼠标", link: "/links/认识计算机/认识鼠标/" },
+          { text: "认识键盘", link: "/links/认识计算机/认识键盘/" },
+          { text: "认识显示器", link: "/links/认识计算机/认识显示器/" },
+          { text: "认识电脑病毒", link: "/links/认识计算机/认识电脑病毒/" },
+        ],
+      },
+],
+    
+},
+}
